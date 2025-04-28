93860a60310a45fa1464215fc8a303e7d3cb9ae1
Gábor Bulla <bulla.gabor@gmail.com>
2025-04-28 17:28:58 +0200

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -1,8 +1,8 @@
 <?php
-// controllers/home.php
+
 ?>
 <section>
   <h2>Üdvözöllek a Receptgyűjteményben!</h2>
   <p>Itt találod a legfinomabb recepteket kategóriák szerint rendezve.</p>
-  <!-- Ide kerül majd a két videó és a Google térkép beágyazása is -->
+  
 </section>
